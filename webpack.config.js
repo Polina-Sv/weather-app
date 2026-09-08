49142fb1c05da92391fd413a0dfcd1ada1f62e1e
Polina-Sv <polinasvyatkina0@gmail.com>
2026-09-08 12:52:09 +0300

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,57 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
+const REPO_NAME = "weather-app";
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-  mode: "development",
+  return {
+    entry: "./src/index.js",
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-      },
+
+    output: {
+      path: path.resolve(__dirname, "dist"), 
+      filename: "bundle.js",
+      clean: true,
+      publicPath: isProduction ? `/${REPO_NAME}/` : "/",
+    },
+
+    mode: isProduction ? "production" : "development",
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
 
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-
-
-  devServer: {
-    static: "./dist",
-    port: 3000,     //http://localhost:3000
-    open: true,
-    hot: true,
-  },
+    devServer: {
+      static: "./dist",
+      port: 3000,
+      open: true,
+      hot: true,
+    },
+  };
 };
